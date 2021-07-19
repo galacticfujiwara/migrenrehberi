4b3082871870dcb2a678448d5a22c0550fedb76b
Kerem Yılmaz <keremy411@gmail.com>
2021-07-19 16:41:00 +0300

    tes
    text

--- helper/db.js
+++ helper/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect(
-    'mongodb+srv://Admin:9wHO0i4SBYm8uqyl@migren.5rk2p.mongodb.net/MigrenDB?retryWrites=true&w=majority',
-    {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true,poolSize:20},
-  );
+ 
   mongoose.connection.on('open', () => {
     console.log('MongoDB: Connected');
   });
